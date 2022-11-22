8ae0ca610e3829b38c2e80216e45609c4c769ec1
Alessia Cecere <alessia.cecere@studenti.unimi.it>
2022-11-22 20:59:32 +0100

    Add license and clustering

---
+++ clustering.py
@@ -0,0 +1,131 @@
+# Copyright (c) 2021 Shivangi0503
+# 
+#
+# This file is licensed under MIT License
+# See https://github.com/Shivangi0503/Wine_Clustering_KMeans/blob/main/LICENSE for more information.
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn import metrics
+from sklearn.model_selection import ParameterGrid
+from sklearn.cluster import KMeans
+
+
+def pca_embeddings(df_scaled):
+    """To reduce the dimensions of the wine dataset we use Principal Component Analysis (PCA).
+    Here we reduce it from 13 dimensions to 2.
+
+    :param df_scaled: scaled data
+    :return: pca result, pca for plotting graph
+    """
+
+    pca_2 = PCA(n_components=2)
+    pca_2_result = pca_2.fit_transform(df_scaled)
+    print('Explained variation per principal component: {}'.format(pca_2.explained_variance_ratio_))
+    print('Cumulative variance explained by 2 principal components: {:.2%}'.format(
+        np.sum(pca_2.explained_variance_ratio_)))
+
+    # Results from pca.components_
+    dataset_pca = pd.DataFrame(abs(pca_2.components_), columns=df_scaled.columns, index=['PC_1', 'PC_2'])
+    print('\n\n', dataset_pca)
+    
+    print("\n*************** Most important features *************************")
+    print('As per PC 1:\n', (dataset_pca[dataset_pca > 0.3].iloc[0]).dropna())
+    print('\n\nAs per PC 2:\n', (dataset_pca[dataset_pca > 0.3].iloc[1]).dropna())
+    print("\n******************************************************************")
+
+    return pca_2_result, pca_2
+
+
+def kmean_hyper_param_tuning(data):
+    """
+    Hyper parameter tuning to select the best from all the parameters on the basis of silhouette_score.
+
+    :param data: dimensionality reduced data after applying PCA
+    :return: best number of clusters for the model (used for KMeans n_clusters)
+    """
+    # candidate values for our number of cluster
+    parameters = [2, 3, 4, 5]
+
+    # instantiating ParameterGrid, pass number of clusters as input
+    parameter_grid = ParameterGrid({'n_clusters': parameters})
+
+    best_score = -1
+    kmeans_model = KMeans()     # instantiating KMeans model
+    silhouette_scores = []
+
+    # evaluation based on silhouette_score
+    for p in parameter_grid:
+        kmeans_model.set_params(**p)    # set current hyper parameter
+        kmeans_model.fit(data)          # fit model on wine dataset, this will find clusters based on parameter p
+
+        ss = metrics.silhouette_score(data, kmeans_model.labels_)   # calculate silhouette_score
+        silhouette_scores += [ss]       # store all the scores
+
+        print('Parameter:', p, 'Score', ss)
+
+        # check p which has the best score
+        if ss > best_score:
+            best_score = ss
+            best_grid = p
+
+    # plotting silhouette score
+    plt.bar(range(len(silhouette_scores)), list(silhouette_scores), align='center', color='#722f59', width=0.5)
+    plt.xticks(range(len(silhouette_scores)), list(parameters))
+    plt.title('Silhouette Score', fontweight='bold')
+    plt.xlabel('Number of Clusters')
+    plt.show()
+
+    return best_grid['n_clusters']
+
+
+def visualizing_results(pca_result, label, centroids_pca):
+    """ Visualizing the clusters
+
+    :param pca_result: PCA applied data
+    :param label: K Means labels
+    :param centroids_pca: PCA format K Means centroids
+    """
+
+    TYPES = ['INFJ',
+         'INTJ',
+         'ENTJ',
+         'INFP',
+         'INTP',
+         'ENFP',
+         'ENTP',
+         'ENFJ',
+         'ISFJ',
+         'ISTJ',
+         'ISTP',
+         'ISFP',
+         'ESFJ',
+         'ESTJ',
+         'ESTP',
+         'ESFP']
+
+    # ------------------ Using Matplotlib for plotting-----------------------
+    x = pca_result[:, 0]
+    y = pca_result[:, 1]
+
+    _, ax = plt.subplots()
+    ax.scatter(x, y, c=label, alpha=0.5, s=200)
+    for i, txt in enumerate(TYPES):
+        ax.annotate(txt, (x[i], y[i]))
+
+    # plt.scatter(x, y, c=label, alpha=0.5, s=200)  # plot different colors per cluster
+
+    plt.title('Personality clusters')
+    plt.xlabel('PCA 1')
+    plt.ylabel('PCA 2')
+
+    # plt.scatter(centroids_pca[:, 0], centroids_pca[:, 1], s=200, linewidths=1.5,
+    #            color='red', edgecolors="black", lw=1.5)
+
+    plt.show()
+
--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -26,12 +26,13 @@ def get_posts(type):
     ).drop(columns=["timestamp"])
     return posts
 
+
 def get_posts_sentiment(type):
     if os.path.exists(f"data/{type}_sentiment.pickle"):
         with open(f"data/{type}_sentiment.pickle", "rb") as f:
             sentiment = pickle.load(f)
         return sentiment
-    else: 
+    else:
         sentiment_pipeline = pipeline(
             "sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english", top_k=None, truncation=True
         )
@@ -41,10 +42,11 @@ def get_posts_sentiment(type):
         posts = get_posts(type)
         for _, post in tqdm(posts.iterrows(), total=len(posts)):
             sentiment[post.post_id] = sentiment_pipeline(post.post_body)[0]
-        
+
         with open(f"data/{type}_sentiment.pickle", "wb") as f:
             pickle.dump(sentiment, f)
-        return sentiment 
+        return sentiment
+
 
 def get_posts_emotions(type):
 
@@ -52,7 +54,7 @@ def get_posts_emotions(type):
         with open(f"data/{type}_emotions.pickle", "rb") as f:
             sentiment = pickle.load(f)
         return sentiment
-    else: 
+    else:
         classifier = pipeline(
             "text-classification", model='bhadresh-savani/distilbert-base-uncased-emotion', top_k=None, truncation=True)
 
@@ -61,10 +63,11 @@ def get_posts_emotions(type):
         posts = get_posts(type)
         for _, post in tqdm(posts.iterrows(), total=len(posts)):
             emotions[post.post_id] = classifier(post.post_body)[0]
-        
+
         with open(f"data/{type}_emotions.pickle", "wb") as f:
             pickle.dump(emotions, f)
-        return emotions 
+        return emotions
+
 
 def get_sentiment_max(type):
     sentiment = get_posts_sentiment(type)
@@ -72,7 +75,7 @@ def get_sentiment_max(type):
 
     for eval in list(sentiment.values()):
         sentiment_max[max(eval, key=lambda x: x["score"])["label"]] += 1
-    
+
     return sentiment_max, len(sentiment)
 
 
@@ -96,6 +99,7 @@ def get_emotions_max(emotions):
         emotions_max[max(eval, key=lambda x: x["score"])["label"]] += 1
     return emotions_max
 
+
 def get_posts_emotion_scores(personality, emotion):
     posts = get_posts(personality)
     emotions = get_posts_emotions(posts)
@@ -108,8 +112,10 @@ def get_posts_emotion_scores(personality, emotion):
 
 
 def get_trait_types(trait):
-    TYPES = ["infj", "intj", "infp", "intp", "istj", "isfj", "istp", "isfp", "enfj", "enfp", "entp", "entj", "estj", "esfj", "estp", "esfp"]
+    TYPES = ["infj", "intj", "infp", "intp", "istj", "isfj", "istp", "isfp",
+             "enfj", "enfp", "entp", "entj", "estj", "esfj", "estp", "esfp"]
     return [type for type in TYPES if trait in type]
+
 
 def get_correlation(trait, non_trait, emotion):
     trait_presence = []
@@ -122,10 +128,9 @@ def get_correlation(trait, non_trait, emotion):
         type_scores = get_posts_emotion_scores(type, emotion)
         emotion_scores.extend(type_scores)
         trait_presence.extend(np.zeros(len(type_scores)))
-    
+
     corr, pvalue = stats.pointbiserialr(trait_presence, emotion_scores)
     return corr, pvalue
-    
 
 
 def get_wordcloud(posts):
@@ -135,13 +140,14 @@ def get_wordcloud(posts):
     return wordcloud
 
 
-
 def get_types_avg_emotion(types):
-    types_sum = dict({"love": 0, "joy": 0, "anger": 0, "sadness": 0, "surprise": 0, "fear": 0})
+    types_sum = dict({"love": 0, "joy": 0, "anger": 0,
+                     "sadness": 0, "surprise": 0, "fear": 0})
 
     for personality in types:
         personality_posts = get_posts(personality)
-        personality_avg = get_emotions_avg(get_posts_emotions(personality_posts), len(personality_posts))
+        personality_avg = get_emotions_avg(get_posts_emotions(
+            personality_posts), len(personality_posts))
         types_sum = dict(Counter(types_sum) + Counter(personality_avg))
 
     return {k: types_sum[k]/len(types) for k in types_sum}
